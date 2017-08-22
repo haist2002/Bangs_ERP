15e490b516e0b4cb8d85742c4f1c61e776acbe5e
haist2002 <haist2002@gmail.com>
2017-08-22 10:17:45 +0900

    초기 커밋.

---
+++ Bangs_ERP/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bangs_ERP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            string text = "HELLO JAVA";
+
+            ViewBag.Text = text;
+            return View();
+        }
+    }
+}
---
+++ Bangs_ERP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bangs_ERP.MvcApplication" Language="C#" %>
